05714fc5984451e30f936ca51f956572f3526239
Paul Verheul <paulverheul@MBP-van-Paul.lan>
2019-05-01 20:19:05 +0200

    Add more and better defined tags and classes to the form, close #6

--- src/src/Cookiebanner.vue
+++ src/src/Cookiebanner.vue
@@ -1,22 +1,29 @@
 <template>
     <div v-if="consentInfo && bannerShouldBeShown && ((consentInfo.consentImplied && isFirstVisit) || (!consentInfo.consentImplied && consentInfo.consentNeeded && !consentInfo.consentSubmitted))" :class="['ccc-banner', 'ccc-banner--' + pluginSettings.bannerPosition]" :style="{ backgroundColor: pluginSettings.bannerColor }">
-        <h3 class="ccc-banner__title" v-html="pluginSettings.bannerTitle"></h3>
-        <p class="ccc-banner__description" v-html="pluginSettings.bannerText"></p>
+        <div class="ccc-banner__text">
+            <h3 class="ccc-banner__title" v-html="pluginSettings.bannerTitle"></h3>
+            <p class="ccc-banner__description" v-html="pluginSettings.bannerText"></p>
+        </div>
 
-        <form method="POST">
-            <input type="hidden" :name="csrfTokenName" :value="csrfTokenValue" />
-            <input type="hidden" name="action" value="complete-cookie-consent/consent/submit" />
-            <div class="ccc-banner__label-container">
-                <label :for="'cookieType-' + type.handle" v-for="type in pluginSettings.cookieTypes" :key="type.handle">
-                    <input v-if="type.required" type="hidden" name="cookieTypes[]" :value="type.handle" value="1" />
-                    <input :id="'cookieType-' + type.handle" type="checkbox" name="cookieTypes[]" :value="type.handle" :checked="type.defaultOn" :disabled="type.required == 1" /> {{ type.name }}
-                </label>
+        <form method="POST" class="ccc-banner__form ccc-form">
+            <div class="ccc-form__inner">
+                <input type="hidden" :name="csrfTokenName" :value="csrfTokenValue" />
+                <input type="hidden" name="action" value="complete-cookie-consent/consent/submit" />
+                <div class="ccc-banner__label-container">
+                    <label :for="'cookieType-' + type.handle" v-for="type in pluginSettings.cookieTypes" :key="type.handle" class="ccc-form__label">
+                        <input v-if="type.required" type="hidden" name="cookieTypes[]" :value="type.handle" value="1" />
+                        <input :id="'cookieType-' + type.handle" type="checkbox" name="cookieTypes[]" :value="type.handle" :checked="type.defaultOn" :disabled="type.required == 1" class="ccc-form__input" />
+                        <span class="ccc-form__label-text">{{ type.name }}</span>
+                    </label>
+                </div>
+
+                <div class="ccc-banner__buttons">
+                    <input type="submit" :value="pluginSettings.primaryButtonText" :style="{ backgroundColor: pluginSettings.primaryButtonBackgroundColor, color: pluginSettings.primaryButtonTextColor }" class="ccc-banner__button ccc-banner__button--primary" />
+                    
+                    <a :href="pluginSettings.secondaryButtonHref" v-if="pluginSettings.showSecondaryButton" :style="{ backgroundColor: pluginSettings.secondaryButtonBackgroundColor, color: pluginSettings.secondaryButtonTextColor   }" class="ccc-banner__button ccc-banner__button--secondary" v-html="pluginSettings.secondaryButtonText" :target="pluginSettings.secondaryButtonOpenInNewTab ? '_blank' : '_self'" rel="noopener nofollow">
+                    </a>
+                </div>
             </div>
-
-            <input type="submit" :value="pluginSettings.primaryButtonText" :style="{ backgroundColor: pluginSettings.primaryButtonBackgroundColor, color: pluginSettings.primaryButtonTextColor }" class="ccc-banner__button" />
-            
-            <a :href="pluginSettings.secondaryButtonHref" v-if="pluginSettings.showSecondaryButton" :style="{ backgroundColor: pluginSettings.secondaryButtonBackgroundColor, color: pluginSettings.secondaryButtonTextColor   }" class="ccc-banner__button ccc-banner__button--secondary" v-html="pluginSettings.secondaryButtonText" :target="pluginSettings.secondaryButtonOpenInNewTab ? '_blank' : '_self'" rel="noopener nofollow">
-            </a>
         </form>
     </div>
 </template>
